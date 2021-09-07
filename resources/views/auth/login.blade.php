14e29c65756a5112bb0e5aa999f5de5c8eb95d1d
naeemliaqatweb <80980012+naeemliaqatweb@users.noreply.github.com>
2021-09-07 06:36:11 -0700

    first

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,11 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <img src="{{asset('img/logo.png')}}" width="250" height="250">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
-
+        
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
@@ -38,11 +32,140 @@
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif --}}
-
+{{-- 
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+ --}}
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Admin Panel</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+
+    <!-- Favicon icon -->
+    <link rel="icon" href="{{asset('admin_asset/images/favicon.ico')}}" type="image/x-icon">
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+    <!-- waves.css -->
+    <link rel="stylesheet" href="{{asset('admin_asset/pages/waves/css/waves.min.css')}}" type="text/css" media="all">
+    <!-- Required Fremwork -->
+    <link rel="stylesheet" type="text/css" href="{{asset('admin_asset/css/bootstrap/css/bootstrap.min.css')}}">
+    <!-- waves.css -->
+    <link rel="stylesheet" href="{{asset('admin_asset/pages/waves/css/waves.min.css')}}" type="text/css" media="all">
+    <!-- themify icon -->
+    <link rel="stylesheet" type="text/css" href="{{asset('admin_asset/icon/themify-icons/themify-icons.css')}}">
+    <!-- font-awesome-n -->
+    <link rel="stylesheet" type="text/css" href="{{asset('admin_asset/css/font-awesome-n.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('admin_asset/css/font-awesome.min.css')}}">
+    <!-- scrollbar.css -->
+    <link rel="stylesheet" type="text/css" href="{{asset('admin_asset/css/jquery.mCustomScrollbar.css')}}">
+    <!-- Style.css -->
+    <link rel="stylesheet" type="text/css" href="{{asset('admin_asset/css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.11.1/css/jquery.dataTables.min.css">
+</head>
+
+</head>
+<body themebg-pattern="theme1">
+  <!-- Pre-loader start -->
+  
+  <!-- Pre-loader end -->
+
+    <section class="login-block">
+        <!-- Container-fluid starts -->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <!-- Authentication card start -->
+                        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+
+                       
+                            <form class="md-float-material form-material" method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="text-center">
+            <img src="{{asset('img/logo.png')}}" width="250" height="250">
+                            </div>
+                            <div class="auth-box card">
+                                <div class="card-block">
+                                    <div class="row m-b-20">
+                                        <div class="col-md-12">
+                                            <h3 class="text-center">Sign In</h3>
+                                        </div>
+                                    </div>
+                                    <div class="form-group form-primary">
+                                        <input  class="form-control" id="email"  type="email" name="email" :value="old('email')" required autofocus >
+                                        <span class="form-bar"></span>
+                                        <label class="float-label">Your Email Address</label>
+                                    </div>
+                                    <div class="form-group form-primary">
+                                        <input type="password" name="password" class="form-control" id="password"  type="password" name="password" required autocomplete="current-password">
+                                        <span class="form-bar"></span>
+                                        <label class="float-label">Password</label>
+                                    </div>
+                                    <div class="row m-t-25 text-left">
+                                        <div class="col-12">
+                                            <div class="checkbox-fade fade-in-primary d-">
+                                                <label>
+                                                    <input type="checkbox" value="">
+                                                    <span class="cr"><i class="cr-icon icofont icofont-ui-check txt-primary"></i></span>
+                                                    <span class="text-inverse">Remember me</span>
+                                                </label>
+                                            </div>
+                                            <div class="forgot-phone text-right f-right">
+                                                <a href="auth-reset-password.html" class="text-right f-w-600"> Forgot Password?</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row m-t-30">
+                                        <div class="col-md-12">
+                                            <button type="submit" name="submit" class="btn btn-primary btn-md btn-block waves-effect waves-light text-center m-b-20">  {{ __('Log in') }}</button>
+                                        </div>
+                                    </div>
+                                   
+                                </div>
+                            </div>
+                        </form>
+                        <!-- end of form -->
+                </div>
+                <!-- end of col-sm-12 -->
+            </div>
+            <!-- end of row -->
+        </div>
+        <!-- end of container-fluid -->
+    </section>
+   
+               <script type="text/javascript" src="{{asset('admin_asset/js/jquery/jquery.min.js')}} "></script>
+    <script type="text/javascript" src="{{asset('admin_asset/js/jquery-ui/jquery-ui.min.js')}} "></script>
+    <script type="text/javascript" src="{{asset('admin_asset/js/popper.js/popper.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('admin_asset/js/bootstrap/js/bootstrap.min.js')}} "></script>
+    <!-- waves js -->
+    <script src="{{asset('admin_asset/pages/waves/js/waves.min.js')}}"></script>
+    <!-- jquery slimscroll js -->
+    <script type="text/javascript" src="{{asset('admin_asset/js/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
+
+    <!-- slimscroll js -->
+    <script src="{{asset('admin_asset/js/jquery.mCustomScrollbar.concat.min.js')}} "></script>
+
+    <!-- menu js -->
+    <script src="{{asset('admin_asset/js/pcoded.min.js')}}"></script>
+    <script src="{{asset('admin_asset/js/vertical/vertical-layout.min.js')}} "></script>
+
+    <script type="text/javascript" src="{{asset('admin_asset/js/script.js')}} "></script>
+    <script type="text/javascript" src="//cdn.datatables.net/1.11.1/js/jquery.dataTables.min.js"></script>
+</body>
+
+</html>
+
+
