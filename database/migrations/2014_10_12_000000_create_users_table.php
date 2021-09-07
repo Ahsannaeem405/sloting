a32609fd4257f88ba9839aab00359102373a2406
naeemliaqatweb <80980012+naeemliaqatweb@users.noreply.github.com>
2021-09-07 06:17:29 -0700

    oky

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->string('number')->nullable();
             $table->string('age')->nullable();
